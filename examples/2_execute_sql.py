3ab5125ad2324a976a446faf88a5ad531604f8c5
hccngu <hccngu@163.com>
2023-11-22 16:08:20 +0800

    debug

--- examples/2_execute_sql.py
+++ examples/2_execute_sql.py
@@ -1,12 +1,14 @@
 from jarvis.agent.openai_agent import OpenAIAgent
 from jarvis.enviroment.old_env import BaseEnviroment
+from jarvis.enviroment.py_env import PythonEnv
 
 '''
 A minimal example for base env and openai agent
 The goal of this example is to demonstrate how agent parse response to get actions, and env execute those actions.
 '''
 
-environment = BaseEnviroment()
+# environment = BaseEnviroment()
+environment = PythonEnv()
 agent = OpenAIAgent(config_path="config.json")
 
 response = '''
@@ -18,12 +20,12 @@ Actions:
 action = agent.extract_action(response, begin_str='<action>', end_str='</action>')
 import time
 for a in action:
-    command = agent.action_lib[a]
+    command = agent.action_lib[a] + '\nprint({tool_name}()())'.format(tool_name=a)
     # print(a, command)
-    print(environment.step(command))
+    print(environment.step(command).result)
     # time.sleep(2)
 
-from jarvis.action_lib.execute_sql import ExecuteSQL
+# from jarvis.action_lib.execute_sql import ExecuteSQL
 
-action = ExecuteSQL()
-action(query='SELECT * FROM railway\nWHERE number="D1000";')
+# action = ExecuteSQL()
+# action(query='SELECT * FROM railway\nWHERE number="D1000";')
