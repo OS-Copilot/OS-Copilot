0e40bed69633a6a2a2fbfb53cfe97bc2359dd259
heroding77 <2441145504@qq.com>
2023-11-28 22:55:15 +0800

    xxx

--- examples/7_code_generation_and_refine.py
+++ examples/7_code_generation_and_refine.py
@@ -5,12 +5,12 @@ from jarvis.agent.linux_skill_amend import LinuxSkillAmend
 from jarvis.agent.linux_invoke_generator import LinuxInvokeGenerator
 '''
 Made By WZM & DZC
-用处：jarvis工具代码生成pipeline
+target: use jarvis lib code to generate pipeline.
 '''
 environment = PythonEnv()
-agent = OpenAIAgent(config_path="./config.json")
-skillCreator = LinuxSkillCreator(config_path="./config.json")
-invokeGenerator = LinuxInvokeGenerator(config_path="./config.json")
+agent = OpenAIAgent(config_path="config.json")
+skillCreator = LinuxSkillCreator(config_path="config.json")
+invokeGenerator = LinuxInvokeGenerator(config_path="config.json")
 
 response = '''
 Thought: To download a file from internet , we can focus on xxx.
@@ -25,12 +25,12 @@ action = agent.extract_action(response, begin_str='<action>', end_str='</action>
 task_description = agent.extract_action(response, begin_str='<description>', end_str='</description>')
 for a,t in zip(action,task_description):
     print(environment.working_dir)
-    msg = skillCreator.format_message(task_name=a,task_description=t,working_dir=environment.working_dir)
+    msg = skillCreator.format_message(task_description=t,working_dir=environment.working_dir)
     code = skillCreator.extract_python_code(msg)
     msg_invoke = invokeGenerator.invoke_generator(code, t)
     invoke = agent.extract_action(msg_invoke, begin_str='<invoke>', end_str='</invoke>')[0]
     code = code + '\n' + invoke
+    print("XXXXXXXXXXXXXXXXXXX")
     print(code)
-
     state = environment.step(code)
     print(state)
