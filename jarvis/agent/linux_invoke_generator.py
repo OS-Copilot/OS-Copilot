230aeaa21b15ef3817c4f9e3b1d19f5b10ff8edb
heroding77 <2441145504@qq.com>
2023-11-28 19:17:51 +0800

    代码调用生成

--- jarvis/agent/linux_invoke_generator.py
+++ jarvis/agent/linux_invoke_generator.py
@@ -9,15 +9,15 @@ You should only respond with the python code in the format as described below:
 1.Class Context: Begin by understanding the context of the Python class provided by the user. This includes grasping the class name and its intended functionality.
 2.Task Description Analysis: Analyze the task description provided to determine the purpose of the class and how it is expected to operate. This will help in identifying the correct method of the class to invoke.
 3.Parameter Details Interpretation: Interpret the parameter details of the __call__ method. This will involve extracting the type of parameters and their role in the method.
-4.Generating Invocation Statement: Construct the __call__ method invocation statement. This includes instantiating the class and passing the appropriate arguments to the __call__ method based on the task description.
-5.Fake Parameter Identification: If the required parameter information (like a URL or file path) is not provided and a placeholder or fake parameter is used, clearly identify and list these as not being actual or valid values.
-6.Output Format: The final output should be enclosed in <invoke></invoke> tags.
+4.Generating Invocation Statement: Construct the __call__ method invocation statement. This includes instantiating the class and passing the appropriate arguments to the __call__ method based on the task description. eg. class_name()(arg1,arg2,....)
+5.Fake Parameter Identification: If the required parameter information (like a URL or file path) is not provided and a placeholder or fake parameter is used, clearly identify and list these as not being actual or valid values.All the fake paramters you list should be separated by comma.If there are no fake parameters,you should give a None.
+6.Output Format: The final output should include two parts:The first one is the invocation statement,which will be enclosed in <invoke></invoke> tags.The second one is all the fake parameters you identified, which will be enclosed in <fake-params></fake-params> tags.
 And the response you write should also follow the following criteria:
 Criteria:
 1.The __call__ method invocation must be syntactically correct as per Python standards.
 2.Clearly identify any fake or placeholder parameters used in the invocation.
 3.Encourage generating a realistic and functional code snippet wherever possible.
-Now you will be provided with the following information, please generate the Python __call__ method invocation statement according to these information :
+Now you will be provided with the following information, please generate the Python __call__ method invocation statement according to these information:
 '''
 _LINUX_USER_INVOKE_GENERATOR_PROMPT = '''
 User's Information:
@@ -89,37 +89,44 @@ class LinuxInvokeGenerator():
 
     
 
-# test = LinuxInvokeGenerator(config_path="../../examples/config.json")
-# print(test.extract_class_name_and_args_description('''
+class_code = '''
                        
-# from jarvis.action.base_action import BaseAction
-# import subprocess
+from jarvis.action.base_action import BaseAction
+import subprocess
 
-# class DownloadAndPlayAudio(BaseAction):
-#     def __init__(self):
-#         self._description = "Download audio from the given link and play it in the system"
+class DownloadAndPlayAudio(BaseAction):
+    def __init__(self):
+        self._description = "Download audio from the given link and play it in the system"
 
-#     def __call__(self, link):
-#         """
-#         Download audio from the given link and play it in the system.
+    def __call__(self, link):
+        """
+        Download audio from the given link and play it in the system.
 
-#         Args:
-#             link (str): The URL of the audio file to be downloaded.
+        Args:
+            link (str): The URL of the audio file to be downloaded.
 
-#         Returns:
-#             None
-#         """
-#         # Download the audio file to the desktop
-#         subprocess.run(["wget", link, "-P", "~/Desktop"])
+        Returns:
+            None
+        """
+        # Download the audio file to the desktop
+        subprocess.run(["wget", link, "-P", "~/Desktop"])
 
-#         # Get the file name from the link
-#         file_name = link.split("/")[-1]
+        # Get the file name from the link
+        file_name = link.split("/")[-1]
 
-#         # Play the audio file
-#         subprocess.run(["xdg-open", f"~/Desktop/{file_name}"])
+        # Play the audio file
+        subprocess.run(["xdg-open", f"~/Desktop/{file_name}"])
 
-# # Example usage
-# # task = DownloadAndPlayAudio()
-# # task("https://example.com/audio.mp3")
-                            
-#                                                    '''))
+# Example usage
+# task = DownloadAndPlayAudio()
+# task("https://example.com/audio.mp3")
+'''
+task_description = '''
+download music from the Internet to the DeskTop, and play it.
+'''
+
+test = LinuxInvokeGenerator(config_path="../../examples/config.json")
+file_path = 'invoke.txt'
+res = test.invoke_generator(class_code, task_description)
+with open(file_path, 'w', encoding='utf-8') as file:
+    file.write(res)
