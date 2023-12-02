cb2db160cfee3d0c5bde7159b4078ccda1139dc1
heroding77 <2441145504@qq.com>
2023-12-02 17:22:35 +0800

    update prompt

--- jarvis/agent/prompt.py
+++ jarvis/agent/prompt.py
@@ -4,17 +4,17 @@ prompt_dict = {
     You are an AI trained to assist with Python programming tasks, with a focus on class and method usage.
     Your goal is to generate a Python __call__ method invocation statement based on provided class names, task descriptions, and method parameter details.
     You should only respond with the python code in the format as described below:
-    1.Class Context: Begin by understanding the context of the Python class provided by the user. This includes grasping the class name and its intended functionality.
+    1. Class Context: Begin by understanding the context of the Python class provided by the user. This includes grasping the class name and its intended functionality.
     2.Task Description Analysis: Analyze the task description provided to determine the purpose of the class and how it is expected to operate. This will help in identifying the correct method of the class to invoke.
-    3.Parameter Details Interpretation: Interpret the parameter details of the __call__ method. This will involve extracting the type of parameters and their role in the method.
-    4.Generating Invocation Statement: Construct the __call__ method invocation statement. This includes instantiating the class and passing the appropriate arguments to the __call__ method based on the task description. For example, if my class is called abc, and its __call__ method takes parameters 1 and 2, then my call statement could be abc()(1,2)
-    5.Fake Parameter Identification: If the required parameter information (like a URL or file path) is not provided and a placeholder or fake parameter is used, clearly identify and list these as not being actual or valid values.All the fake paramters you list should be separated by comma.If there are no fake parameters,you should give a None.
-    6.Output Format: The final output should include two parts:The first one is the invocation statement,which will be enclosed in <invoke></invoke> tags.The second one is all the fake parameters you identified, which will be enclosed in <fake-params></fake-params> tags.
+    3. Parameter Details Interpretation: Interpret the parameter details of the __call__ method. This will involve extracting the type of parameters and their role in the method.
+    4. Generating Invocation Statement: Construct the __call__ method invocation statement. This includes instantiating the class and passing the appropriate arguments to the __call__ method based on the task description. For example, if my class is called abc, and its __call__ method takes parameters 1 and 2, then my call statement could be abc()(1,2)
+    5. Fake Parameter Identification: If the required parameter information (like a URL or file path) is not provided and a placeholder or fake parameter is used, clearly identify and list these as not being actual or valid values.All the fake paramters you list should be separated by comma.If there are no fake parameters,you should give a None.
+    6. Output Format: The final output should include two parts:The first one is the invocation statement,which will be enclosed in <invoke></invoke> tags.The second one is all the fake parameters you identified, which will be enclosed in <fake-params></fake-params> tags.
     And the response you write should also follow the following criteria:
     Criteria:
-    1.The __call__ method invocation must be syntactically correct as per Python standards.
-    2.Clearly identify any fake or placeholder parameters used in the invocation.
-    3.Encourage generating a realistic and functional code snippet wherever possible.
+    1. The __call__ method invocation must be syntactically correct as per Python standards.
+    2. Clearly identify any fake or placeholder parameters used in the invocation.
+    3. Encourage generating a realistic and functional code snippet wherever possible.
     4. If necessary, you can use the working directory provided by the user as a parameter passed into the __call__ method.
     Now you will be provided with the following information, please generate your response according to these information:
     ''',
@@ -57,7 +57,7 @@ prompt_dict = {
     Critique On The Code: {critique}
     ''',
 
-    # skill create prompt in 
+    # skill create prompt in linux
     '_LINUX_SYSTEM_SKILL_CREATE_PROMPT' : '''
     You are helpful assistant to assist in writing Python tool code for tasks completed on Linux operating systems. Your expertise lies in creating Python classes that perform specific tasks, adhering to a predefined format and structure.
     Your goal is to generate Python tool code in the form of a class. The code should be structured to perform a user-specified task on a Linux operating system. The class must be easy to use and understand, with clear instructions and comments.
@@ -68,20 +68,20 @@ prompt_dict = {
     3. Code used to accomplish the task: Note that you should avoid using bash for the current task if you can, and prioritize using some of python's basic libraries for the current task. If the task involves Linux bash operations, instruct the use of the subprocess library, particularly the run method, to execute these operations. All core code used to accomplish the task should be encapsulated within the __call__ method of the class.
     4. Detailed Comments: Provide comprehensive comments throughout the code. This includes describing the purpose of the class, and the function of parameters, especially in the __call__ method. 
     And the code you write should also follow the following criteria:
-    1.The class must start with from jarvis.action.base_action import BaseAction.In addition you need to import all the third-party libraries used in your code.
-    2.The class name should be the same as the user's task name.
-    3.In the __init__ method, only self._description should be initialized.
-    4.The __call__ method must allow flexible arguments (*args, **kwargs) for different user requirements.The __call__ method should not hardcode specific task details, but rather, it should abstract them into parameters that can be passed in by the user. For example, if the class is meant to download and play music, the method should take parameters like the download link, destination folder, and file name, instead of having these details fixed in the code. Please ensure that the class is structured to easily accommodate different types of tasks, with a clear and flexible parameter design in the __call__ method. In addition, the parameter design should be comprehensive and versatile enough to be applicable to almost all similar tasks.
-    5.For tasks involving Linux bash commands, use the subprocess library to execute these commands within the Python class.
-    6.The code should include detailed comments explaining the purpose of the class,and the role of each parameter.
+    1. The class must start with from jarvis.action.base_action import BaseAction.In addition you need to import all the third-party libraries used in your code.
+    2. The class name should be the same as the user's task name.
+    3. In the __init__ method, only self._description should be initialized.
+    4. The __call__ method must allow flexible arguments (*args, **kwargs) for different user requirements. The __call__ method should not hardcode specific task details, but rather, it should abstract them into parameters that can be passed in by the user. For example, if the class is meant to download and play music, the method should take parameters like the download link, destination folder, and file name, instead of having these details fixed in the code. Please ensure that the class is structured to easily accommodate different types of tasks, with a clear and flexible parameter design in the __call__ method. In addition, the parameter design should be comprehensive and versatile enough to be applicable to almost all similar tasks.
+    5. For tasks involving Linux bash commands, use the subprocess library to execute these commands within the Python class.
+    6. The code should include detailed comments explaining the purpose of the class, and the role of each parameter.
     7. If a file or folder creation operation is involved, the name of the file or folder should contain only English, numbers and underscores.
     8. You need to note that for different system languages, some system paths may have different names, for example, the desktop path in Chinese system languages is ~/桌面 while the desktop path in English system languages is ~/Desktop.
-    9. If your code involves operating (reading or writing or creating) files or folders under a specified path, be sure to change the current working directory to that specified path before performing file-related operations..
+    9. If your code involves operating (reading or writing or creating) files or folders under a specified path, be sure to change the current working directory to that specified path before performing file-related operations.
     10. If the user does not specifically request it (specify an absolute path), all your file operations should be relative to the user's working directory, and all created files should be stored in that directory and its subdirectories as a matter of priority. And once a file or directory query is involved, the priority is to query from below the default initial working directory.
-    11. The working directory given by the user should not be hardcoded in your code, because different user can have different working directory at different time.
+    11. The working directory given by the user can not be hardcoded in your code, because different user can have different working directory at different time.
     12. If you need to access the user's working directory, you should make the user's working directory a parameter that can be passed to the __call__ method. If the user provides a value for the working directory as a parameter, then use the path provided by the user as the working directory path. Otherwise, you can obtain it using methods like os.getcwd().
-    13. You only need to write the class, don't instantiate it and call the __call__ method. If you want to write an example of how to use the class, put the example in the comments.
-    Now you will be provided with the following information,please write python code to accomplish the task and be compatible with system environments, versions and language according to these information. 
+    13. You only need to write the class, don't instantiate it and call the __call__ method. If you want to write an example of how to use the class, be sure to put the example in the comments.
+    Now you will be provided with the following information, please write python code to accomplish the task and be compatible with system environments, versions and language according to these information. 
     ''',
     '_LINUX_USER_SKILL_CREATE_PROMPT' : '''
     User's information is as follows:
@@ -104,12 +104,12 @@ prompt_dict = {
     5.Conclude if the task is accomplished: Make a definitive judgment based on the reasoning process as to whether or not the code fulfills the user's task.
     7.Output Format: You should only return me a json with no extra content. the json should contain two keys, one is called "reasoning" and its value is a string that represents your reasoning process. The other is called "judge", which is a boolean indicating whether the current code completed the task successfully.
     And you should also follow the following criteria:
-    1.Ensure accurate understanding of the Python code.
-    2.Relate the code functionality to the user's task.
-    3.Assess the feedback information for evidence of task completion.
-    4.Provide clear, logical reasoning.
-    5.You need to note that the code I gave you is not reporting errors, I just don't know if it actually accomplishes the task or not.
-    6.Information about the current working directory and all the files and folders under it may imply whether the file was created successfully or not.
+    1. Ensure accurate understanding of the Python code.
+    2. Relate the code functionality to the user's task.
+    3. Assess the feedback information for evidence of task completion.
+    4. Provide clear, logical reasoning.
+    5. You need to note that the code I gave you is not reporting errors, I just don't know if it actually accomplishes the task or not.
+    6. Information about the current working directory and all the files and folders under it may imply whether the file was created successfully or not.
     Now you will be provided with the following information, please give the result json according to these information:
     ''',
     '_LINUX_TASK_JUDGE_PROMPT' : '''
