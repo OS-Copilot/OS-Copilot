7e61cf4755044cc744cc529df02c9f6b1bd05dff
heroding77 <2441145504@qq.com>
2023-12-26 22:07:13 +0800

    xxx

--- working_dir/code/test.py
+++ working_dir/code/test.py
@@ -1,16 +1,23 @@
-# import json
+from jarvis.core.tool_request_util import ToolRequestUtil
 
-# dict =         {
-#             'retrieve_files' : {
-#                 'name': 'retrieve_files',
-#                 'description': 'retrieve the txt text in the folder call document in the working directory. If the text contains the word "agent", save the path of the text file into the list, and return.',
-#                 'dependencies': []
-#             },
-#             'organize_files' : {
-#                 'name': 'organize_files',
-#                 'description': 'put the retrieved files into a folder named agent based on the file path list obtained by executing the previous task.',
-#                 'dependencies': ['retrieve_files']
-#             }    
-#         } 
+# Initialize the ToolRequestUtil
+tool_request_util = ToolRequestUtil()
 
-# print(json.dumps(dict))
+# Define the API path and the method to be used
+api_path = '/tools/bing/image_search'
+method = 'get'
+
+# Define the parameters for the API call
+params = {
+    "query": "East China Normal University",
+    "top_k": 3
+}
+
+# Define the content type
+content_type = 'application/json'
+
+# Make the API call using the ToolRequestUtil
+response = tool_request_util.request(api_path, method, params=params, content_type=content_type)
+
+# Print the return value of the API call
+print(response)
