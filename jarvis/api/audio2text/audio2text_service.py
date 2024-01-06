89178853eabb007088717a053d384fb465bddb10
tamiflu233 <1714118250@qq.com>
2024-01-06 21:16:47 +0800

    音频api

--- jarvis/api/audio2text/audio2text_service.py
+++ jarvis/api/audio2text/audio2text_service.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, HTTPException, File, UploadFile,Depends
 from pydantic import BaseModel,Field
 from typing import Optional
 from .audio2text import Audio2TextTool
-
-
+import io
+import os
+import shutil
 router = APIRouter()
 
 whisper_api = Audio2TextTool()
@@ -17,9 +18,13 @@ class AudioTextQueryItem(BaseModel):
 @router.post("/tools/audio2text")
 async def audio2text(item: AudioTextQueryItem = Depends()):
     try:
-        #  # 读取上传的文件
-        # contents = await item.file.read()
-        caption = whisper_api.caption(audio_file=item.file)
+        # 创建一个临时文件来保存上传的音频
+        with open(item.file.filename, "wb") as buffer:
+            shutil.copyfileobj(item.file.file, buffer)
+        with open(item.file.filename, "rb") as audio:
+            caption = whisper_api.caption(audio_file=audio)
+        # 清理临时文件
+        os.remove(item.file.filename)
         return {"text": caption}
     except RuntimeError as e:
         raise HTTPException(status_code=500, detail=str(e))
