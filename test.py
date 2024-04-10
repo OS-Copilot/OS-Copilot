53bef453642b93641398c3bfafb5effd3f833bae
heroding77 <2441145504@qq.com>
2024-04-10 02:40:59 +0000

    add test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+from oscopilot import FridayExecutor, ToolManager
+from oscopilot.prompts.friday_pt_new import prompt
+from oscopilot.utils import setup_config
+
+args = setup_config()
+prompt = prompt["execute_prompt"]
+excutor = FridayExecutor(prompt, ToolManager)
+task_name, task_description, pre_tasks_info, relevant_code = "move_files", "Move any text file located in the working_dir/document directory that contains the word 'agent' to a new folder named 'agent' ", "", ""
+code, invoke = excutor.generate_tool(task_name, task_description, pre_tasks_info, relevant_code)
+print(code, '\n', invoke)
+
