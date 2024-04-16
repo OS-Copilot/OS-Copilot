c5ee8b1b919809fb80219d449c6541374afb7778
LividWo <361456446@qq.com>
2024-04-16 10:41:10 +0800

    remove .env

--- quick_start.py
+++ quick_start.py
@@ -4,7 +4,6 @@ from oscopilot import FridayExecutor, FridayPlanner, FridayRetriever
 from oscopilot.utils import setup_config, setup_pre_run
 
 args = setup_config()
-print(args.sheet_task_id)
 if not args.query:
     args.query = "Copy any text file located in the working_dir/document directory that contains the word 'agent' to a new folder named 'agents' "
 task = setup_pre_run(args)
