064c7e4752fb90c7b1493fd0ba8721289bc91db2
heroding77 <heroding77@qq.com>
2024-05-08 06:41:51 +0000

    update friday run

--- oscopilot/agents/friday_agent.py
+++ oscopilot/agents/friday_agent.py
@@ -57,15 +57,18 @@ class FridayAgent(BaseAgent):
         print("The task list obtained after planning is: {}".format(sub_tasks_list))
 
         while self.planner.sub_task_list:
-            sub_task = self.planner.sub_task_list.pop(0)
-            execution_state = self.executing(sub_task, task)
-            isTaskCompleted, isReplan = self.self_refining(sub_task, execution_state)
-            if isReplan: continue
-            if isTaskCompleted:
-                print("The execution of the current sub task has been successfully completed.")
-            else:
-                print("{} not completed in repair round {}".format(sub_task, self.config.max_repair_iterations))
-                # sys.exit()
+            try:
+                sub_task = self.planner.sub_task_list.pop(0)
+                execution_state = self.executing(sub_task, task)
+                isTaskCompleted, isReplan = self.self_refining(sub_task, execution_state)
+                if isReplan: continue
+                if isTaskCompleted:
+                    print("The execution of the current sub task has been successfully completed.")
+                else:
+                    print("{} not completed in repair round {}".format(sub_task, self.config.max_repair_iterations))
+                    break
+            except Exception as e:
+                print("Current task execution failed. Error: {}".format(str(e)))
                 break
 
     def self_refining(self, tool_name, execution_state: ExecutionState):
